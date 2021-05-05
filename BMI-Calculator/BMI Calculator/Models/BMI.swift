40ee565239e2ee409f042620928e79cbf0b29bc2
ajaykalaga <83403761+ajaykalaga@users.noreply.github.com>
2021-05-05 09:40:09 +0530

    Add files via upload

---
+++ BMI-Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//Created by Ajay Sharma on 05/04/2021
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
